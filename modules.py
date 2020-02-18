dd6db15e4a8db0ca702a545fb879ee77c6432765
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-02-17 20:42:39 -0700

    Delete modules.py

--- modules.py
+++
@@ -1,9 +0,0 @@
-import keyword
-
-def contains_keyword(*args):
-	return any(arg for arg in args if keyword.iskeyword(arg))
-
-
-print(contains_keyword("Noyon", "Hypnotic eye"))#False
-print(contains_keyword("for", "if", "nothing"))#True
-
