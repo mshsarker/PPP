96cc245fffeb2d570e01c7e1af8708ad34543986
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-02-25 18:01:28 -0700

    Add files via upload

---
+++ Built-In Functions/colorful_text.py
@@ -0,0 +1,19 @@
+import pyfiglet
+from termcolor import colored
+
+
+def print_art(text, color):
+	default_colors = ('grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan','white')
+	if color not in default_colors:
+		color = 'red'
+
+	art = pyfiglet.figlet_format(text)
+	colorful_art = colored(art, color = color)
+	print(colorful_art)
+
+
+
+
+text = input("What do you want to print?")
+color = input("For that reason what color do you want to use?")
+print_art(text,color)
