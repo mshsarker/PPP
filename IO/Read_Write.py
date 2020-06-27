dad9d488e5f6b28a7f2396803be303cff8369595
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-06-27 11:19:46 -0500

    Add files via upload

---
+++ IO/Read_Write.py
@@ -0,0 +1,16 @@
+'''
+copy('story.txt', 'story_copy.txt') # None
+# expect the contents of story.txt and story_copy.txt to be the same
+'''
+
+def copy(file_name, new_file_name):
+    with open(file_name) as file:
+    	text = file.read()
+
+    with open(new_file_name, 'w') as new_file:
+    	new_file.write(text)
+
+
+copy('story.txt', 'story_copy.txt')
+
+
