2f4484ea6600fa2804b5fb985f8ab656e93f8605
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-08-05 10:32:51 -0500

    Add files via upload

---
+++ Misc./reverse.py
@@ -0,0 +1,15 @@
+'''
+reverse_string('awesome') # 'emosewa'
+reverse_string('Colt') # 'tloC'
+reverse_string('Elie') # 'eilE'
+'''
+
+# add whatever parameters you deem necessary - good luck!
+def reverse_string(arg):
+    print(arg[::-1])
+
+
+reverse_string("Bang")
+reverse_string('awesome') # 'emosewa'
+reverse_string('Colt') # 'tloC'
+reverse_string('Elie') # 'eilE'
