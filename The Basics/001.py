e25e3ce8a20fe9daa269fa251670d7ac58a1be9d
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-08-11 09:26:31 -0500

    Add files via upload

---
+++ The Basics/001.py
@@ -0,0 +1,10 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book : affiliate link amazon
+
+# 001 Ask for the user’s first name and display the output message 
+# Hello [First Name]
+
+First_Name = input("Hey Dude! What is your first Name? ")
+print("Hello" , First_Name)
+
+
---
+++ The Basics/002.py
@@ -0,0 +1,11 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book : affiliate link amazon
+
+# 002 Ask for the user’s first name and then ask for their surname and display the output message 
+# Hello [First Name] [Surname]
+
+First_Name = input("Hey Dude! What is your first Name? ")
+Last_Name = input("What is your last Name? ")
+print("Hello" , First_Name, Last_Name)
+
+
