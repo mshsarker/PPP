8361fc4c2033440558b9582878ae6637a0de7830
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-08-15 11:08:35 -0500

    Add files via upload

---
+++ The Basics/010.py
@@ -0,0 +1,7 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book 
+
+#  10 There are 2.204 pounds in a kilogram. Ask the user to enter a weight in kilograms and convert it to pounds.
+kg = int(input("What is your weight in kg? "))
+lbs = round(kg * 2.204,2)
+print(f"You are {lbs} lb ")
