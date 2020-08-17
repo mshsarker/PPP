1097f603150055321794bc13a35b5f03b1e5154f
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-08-17 12:36:31 -0500

    Add files via upload

---
+++ The Basics/13.py
@@ -0,0 +1,9 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book 
+
+#  013 Ask the user to enter a number that is under 20. If they enter a number that is 20 or more, display the message “Too high”, otherwise display “Thank you”.
+T20 = int(input("Please write an integer below 20\n"))
+if T20 > 20:
+	print("Too high")
+else:
+	print("Thank you!!")
