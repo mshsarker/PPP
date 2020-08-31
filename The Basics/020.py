0962f9439301f8d738daaa5ff50b9fde523b77be
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-08-31 13:41:45 -0500

    Add files via upload

---
+++ The Basics/020.py
@@ -0,0 +1,8 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book 
+
+# 020 Ask the user to enter their first name and then display the length of their name
+
+name = input("Hello! What is your first name?\n")
+length = len(name)
+print(f"You have {length} characters in your name")
