6f0b77799b929b8d3661c90db3553720e72b5d80
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-01 10:42:22 -0500

    Add files via upload

---
+++ The Basics/024.py
@@ -0,0 +1,6 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+##  024 Ask the user to type in any word and display it in upper case.
+word = input("Write a word that is in your mind at this moment\n")
+print(f"Welcome {word.upper()}")
