b84e46faa98323f3cde5c928a8332a998627b260
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-03 11:47:17 -0500

    Add files via upload

---
+++ Maths/028.py
@@ -0,0 +1,8 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+##  028 Update program 027 so that it will display the answer to two decimal places.
+
+num = float(input("Write a number: which has a lot of decimal points in it\n"))
+times = num * 2
+print(f"Double of {num} is {round(times,2)}..Yayy!!")
