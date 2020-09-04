26a6e45f5cd637aaca09c9551c3f9c791fe12c87
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-04 11:24:15 -0500

    Add files via upload

---
+++ The Basics/027.py
@@ -0,0 +1,8 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+##  027 Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.
+
+num = float(input("Write a number: which has a lot of decimal points in it\n"))
+times = num * 2
+print(f"Double of {num} is {times}..Yayy!!")
