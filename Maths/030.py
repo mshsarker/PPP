5a39b8777ecdfdacb3bbdab9cf82946492af31ae
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-04 11:24:59 -0500

    Add files via upload

---
+++ Maths/030.py
@@ -0,0 +1,9 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+##   030 Display pi (π) to five decimal places.
+
+import math
+
+pi = math.pi
+print(f"PI to the five decimal number is {round(pi, 5)}")
