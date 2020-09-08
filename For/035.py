1e8ad10a08493ff4fe3593cd71007a2c03f139bf
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-08 13:20:46 -0500

    Add files via upload

---
+++ For/035.py
@@ -0,0 +1,9 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 035 Ask the user to enter their name and then display their name three times.
+
+name = input("Please enter your name \n")
+
+for i in range(1,4):
+	print(name)
