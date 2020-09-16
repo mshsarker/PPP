43ad85ea0561bda4344fbfee6cbf8f4854fb97c5
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-16 09:11:59 -0500

    Add files via upload

---
+++ While Loop/046.py
@@ -0,0 +1,37 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 046 Ask the user to enter a number. Keep asking until they enter a value over 5 and then display the message “The last number you entered was a [number]” and stop the program.
+
+num = 0
+while num <= 5:
+	num = int(input("Please enter an integer"))
+print(f"The last number you have entered is {num}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
