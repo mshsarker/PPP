651b441029344e894cec32d2235e376c17639698
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-16 09:11:12 -0500

    Add files via upload
    Guess! What?

---
+++ While Loop/045.py
@@ -0,0 +1,38 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 045 Set the total to 0 to start with. While the total is 50 or less, ask the user to input a number. Add that number to the total and print the message “The total is... [total]”. Stop the loop when the total is over 50.
+
+total = 0
+while total < 50 :
+	num = int(input("Enter an integer which is below 50\n"))
+	total += num
+	print(f"The total is {total}, Gotcha ? ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
