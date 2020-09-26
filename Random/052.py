bf1e7d0bcc218b301b2cf2bfeea2863cdd1c10b5
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-26 12:58:46 -0500

    Add files via upload

---
+++ Random/052.py
@@ -0,0 +1,18 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 052 Display a random integer between 1 and 100 inclusive.
+
+import random
+
+integer = random.randint(1,100)
+print(integer)
+
+ 
+
+ 
+
+
+
+
+
