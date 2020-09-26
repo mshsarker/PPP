e9223d304c271caa73d1ac5582790846a1241969
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-26 12:59:17 -0500

    Add files via upload

---
+++ Random/053.py
@@ -0,0 +1,19 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 053 Display a random fruit from a list of five fruits.
+
+import random
+
+fruits = ["Mango", "Lichi", "Pineapple", "Orange", "Apple"]
+
+fruit = random.choice(fruits)
+print(fruit)
+ 
+
+ 
+
+
+
+
+
