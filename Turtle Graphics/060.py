f3d34d7b20f559dbcf32d6246cfc55588bff04e8
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-29 10:51:52 -0500

    Add files via upload

---
+++ Turtle Graphics/060.py
@@ -0,0 +1,12 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 060 Draw a square.
+
+import turtle
+
+for i in range(0,4):
+	turtle.forward(100)
+	turtle.right(90)
+
+turtle.exitonclick()
