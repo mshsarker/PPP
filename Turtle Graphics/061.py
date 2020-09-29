23e57caa6596bff8ef06d750e416c4ecf1df6f28
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-09-29 10:52:12 -0500

    Add files via upload

---
+++ Turtle Graphics/061.py
@@ -0,0 +1,13 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 061 Draw a triangle.
+
+import turtle
+
+
+for i in range(0,3):
+	turtle.forward(200)
+	turtle.right(120)
+
+turtle.exitonclick()
