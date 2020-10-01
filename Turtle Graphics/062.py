c5a9f3c0f3c684c32285efaeccdf6be2517d8ef1
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-10-01 10:53:18 -0500

    Add files via upload

---
+++ Turtle Graphics/062.py
@@ -0,0 +1,12 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 062 Draw a circle.
+
+import turtle
+
+for i in range(0,360):
+	turtle.forward(2)
+	turtle.right(1)
+
+turtle.exitonclick()
