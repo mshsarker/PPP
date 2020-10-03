bcd95b9a50c45ac9221ec83bd71545573c05f129
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-10-03 10:13:19 -0500

    Add files via upload

---
+++ Turtle Graphics/064.py
@@ -0,0 +1,14 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 064 Draw a five-pointed star.
+
+
+import turtle
+
+
+for i in range(0,5):
+	turtle.forward(100)
+	turtle.right(144)
+
+turtle.exitonclick()
