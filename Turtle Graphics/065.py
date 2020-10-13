e270d1212e1626aeb7eb02479a7d28f53670035c
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-10-13 09:57:36 -0500

    Add files via upload

---
+++ Turtle Graphics/065.py
@@ -0,0 +1,58 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+## 065 Write the numbers as shown below, starting at the bottom of the number one. 1 2 3
+
+import turtle
+
+# cration of 1
+turtle.right(90)
+turtle.forward(100)
+
+
+
+# Moving to home position
+turtle.penup()
+turtle.left(180)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(30)
+turtle.pendown()
+
+
+# Creation of 2
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+
+
+# Moving to home position
+turtle.penup()
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(30)
+turtle.pendown()
+
+
+# Creation of 3
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(180)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+
+
+turtle.exitonclick()
