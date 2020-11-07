62f7d1d05e10ca4034c1112598d6a2fe3bc5ed90
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2020-11-07 10:23:23 -0600

    Add files via upload

---
+++ Dictionaries/071.py
@@ -0,0 +1,11 @@
+## My solutions to the 150 Challenges by Nichola Lacey
+## Get this wonderful book to know with the explanations and quick tips.
+
+##  071 Create a list of two sports. Ask the user what their favourite sport is and add this to the end of the list. Sort the list and display it.
+
+sports = ["Tennis", "Football"]
+add = input("What is your favourite sport?   ")
+sports.append(add)
+sports.sort()
+print(sports)
+
