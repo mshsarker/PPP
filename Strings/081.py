9082b9e632485304f32a6db908fece7ced45243d
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2021-01-08 09:54:50 -0600

    Add files via upload
    Hell yeah!

---
+++ Strings/081.py
@@ -0,0 +1,6 @@
+# Ask the user to type in their favourite school subject. Display it with “-” after each letter, e.g. S-p-a-n-i-s-h-.
+
+msg = input("Write something whatever in your mind right now:	")
+
+for i in msg:
+	print(i, end = "*")
