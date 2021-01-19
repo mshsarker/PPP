f1f7b78a63a9ea9b9ff4d6d9ff2bf576878c908a
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2021-01-19 07:41:15 -0600

    Add files via upload

---
+++ Strings/084.py
@@ -0,0 +1,9 @@
+#  084 Ask the user to type in their postcode. Display the first two letters in uppercase.
+
+postcode = input("Please write down your postcode in this format: texas77705:	")
+bold = postcode[0:2]
+print(bold.upper())
+
+
+
+
